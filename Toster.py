6a2a596dc236f34b5f5cd1c46ae8f39716f2f793
ToziRTI <filen348@gmail.com>
2024-04-12 22:21:40 +0300

    Tostertort

--- Toster.py
+++ Toster.py
@@ -1,11 +1,12 @@
-def root (name):
-    if name == 'Journey':
-        print('0')
-    elif name == 'lox':
-        print('1')
-    else:
-        print('2')
+def calculate_quarterly_payment(monthly_payments):
+    if len(monthly_payments) != 3:
+        return "Ошибка: Необходимо передать ровно 3 месячных платежа"
 
-        what=input('What do you want to do?')
-        print(name)
-        #rlo
+    quarterly_payment = sum(monthly_payments)
+    return quarterly_payment
+
+
+# Пример использования:
+monthly_payments = [1000, 1200, 1100]
+quarterly_payment = calculate_quarterly_payment(monthly_payments)
+print("Квартальный платеж составляет:", quarterly_payment)
