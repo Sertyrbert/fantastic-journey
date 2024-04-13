019033d327b688628a0434c668b32f267c7d979c
Sertyrbert <yuriev.khv@mail.ru>
2024-04-13 17:11:47 +0300

    push12

--- pfp.py
+++ pfp.py
@@ -11,49 +11,50 @@ import os
 
 nk = 2
 
-# Загрузка данных
-data = pd.read_csv('суд.csv')
-
-# Удаление ненужных столбцов
-data.drop(['slctn_nmbr', 'client_id', 'npo_account_id', 'frst_pmnt_date', 'lst_pmnt_date_per_qrtr'], axis=1,
-          inplace=True)
-
-# Заполнение пропущенных значений
-data.fillna(0, inplace=True)
-
-# Преобразование всех категориальных столбцов к строковому типу
-categorical_cols = ['pmnts_type', 'year', 'quarter', 'gender', 'phone_number', 'email', 'lk', 'assignee_npo',
-                    'assignee_ops', 'postal_code', 'region', 'citizen', 'fact_addrss', 'appl_mrkr', 'evry_qrtr_pmnt']
-for col in categorical_cols:
-    data[col] = data[col].astype(str)
-
-# Кодирование категориальных признаков
-label_encoders = {}
-for col in categorical_cols:
-    le = LabelEncoder()
-    data[col] = le.fit_transform(data[col])
-    label_encoders[col] = le
-
-# Разделение данных на признаки и целевую переменную
-X = data.drop('churn', axis=1)
-y = data['churn']
-
-gt = random.randint(1, 100)
-
-# Разделение данных на обучающую и тестовую выборки
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=gt)
-
-# Масштабирование признаков
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
-
 for i in range(10):
     model_file = "model_file.txt"
     if os.path.exists(model_file):
         booster = lgb.Booster(model_file=model_file)
         print('I\'m here!')
     else:
+        # Загрузка данных
+        data = pd.read_csv('суд.csv')
+
+        # Удаление ненужных столбцов
+        data.drop(['slctn_nmbr', 'client_id', 'npo_account_id', 'frst_pmnt_date', 'lst_pmnt_date_per_qrtr'], axis=1,
+                  inplace=True)
+
+        # Заполнение пропущенных значений
+        data.fillna(0, inplace=True)
+
+        # Преобразование всех категориальных столбцов к строковому типу
+        categorical_cols = ['pmnts_type', 'year', 'quarter', 'gender', 'phone_number', 'email', 'lk', 'assignee_npo',
+                            'assignee_ops', 'postal_code', 'region', 'citizen', 'fact_addrss', 'appl_mrkr',
+                            'evry_qrtr_pmnt']
+        for col in categorical_cols:
+            data[col] = data[col].astype(str)
+
+        # Кодирование категориальных признаков
+        label_encoders = {}
+        for col in categorical_cols:
+            le = LabelEncoder()
+            data[col] = le.fit_transform(data[col])
+            label_encoders[col] = le
+
+        # Разделение данных на признаки и целевую переменную
+        X = data.drop('churn', axis=1)
+        y = data['churn']
+
+        gt = random.randint(1, 100)
+
+        # Разделение данных на обучающую и тестовую выборки
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=gt)
+
+        # Масштабирование признаков
+        scaler = StandardScaler()
+        X_train = scaler.fit_transform(X_train)
+        X_test = scaler.transform(X_test)
+
         # Создание и обучение модели
         clf = LGBMClassifier()
         clf.fit(X_train, y_train)
