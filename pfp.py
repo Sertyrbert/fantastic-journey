534e40f4728e65bc6b1e72ef1a1062c4fa6170b1
Scorpionguy <lapinfedor11@gmail.com>
2024-04-14 10:05:51 +0300

    Предикт, сверка

--- pfp.py
+++ pfp.py
@@ -10,10 +10,11 @@ import lightgbm as lgb
 import os
 
 randA = 1
-randB = 1000000
-repeats = 100
+randB = 1000
+repeats = 30
 old_score = 0
-train_File = 'train.csv'
+train_File = 'test.csv'
+sample_File = 'sample_submission.csv'
 model_file = "trained_model2.txt"
 
 #нужно сделать чтобы через простенький интерфейс заполнялись данные repeats & train_File
@@ -30,7 +31,7 @@ with open('file.txt', 'r+') as file:
     file.truncate(0)
 # Загрузка данных
 data = pd.read_csv(train_File)
-
+data2 = pd.read_csv(sample_File)
 # Удаление ненужных столбцов
 data.drop(['slctn_nmbr', 'client_id', 'npo_account_id', 'frst_pmnt_date', 'lst_pmnt_date_per_qrtr'], axis=1,
           inplace=True)
@@ -52,8 +53,10 @@ for col in categorical_cols:
     label_encoders[col] = le
 
 # Разделение данных на признаки и целевую переменную
+data = data.merge(data2[['churn']], left_index=True, right_index=True)
 X = data.drop('churn', axis=1)
 y = data['churn']
+
 
 gt = random.randint(randA, randB)
 
@@ -64,52 +67,51 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
-clf = LGBMClassifier(n_jobs=-1)
-for i in range(repeats):
+clf = LGBMClassifier(num_leaves=2, max_depth=4, learning_rate=0.1, n_estimators=repeats)
 
-    if os.path.exists(model_file):
+if os.path.exists(model_file):
         gt = random.randint(randA, randB)
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=gt)
-        booster = lgb.Booster(model_file=model_file)
+        booster = lgb.Booster(model_file=model_file, )
         booster.refit(X_train, y_train)
         #booster.save_model(model_file)
         print('I\'m here!')
         wtw = True
-    else:
+else:
         # Создание и обучение модели
         clf.fit(X_train, y_train)
         clf.booster_.save_model(model_file)
         wtw = False
 
     # Прогнозирование на тестовой выборке
-    if wtw:
+if wtw:
         y_pred = booster.predict(X_test)
-    else:
+else:
         y_pred = clf.predict(X_train)
 
 
-    param_grid = {
+param_grid = {
         'n_estimators': [50, 100, 150],
         'max_depth': [3, 5, 7]
     }
 
     # Создание объекта GridSearchCV
-    grid_search = GridSearchCV(LGBMClassifier(), param_grid, cv=5, n_jobs=4, scoring='f1')
+grid_search = GridSearchCV(LGBMClassifier(), param_grid, cv=5, n_jobs=4, scoring='f1')
 
     # Обучение объекта GridSearchCV
-    grid_search.fit(X_train, y_train)
+grid_search.fit(X_train, y_train)
 
     # Оценка производительности на отложенном тестовом наборе
-    f1_score = grid_search.best_score_
+f1_score = grid_search.best_score_
 
-    print('F1 score:', f1_score.mean())
-    if f1_score.mean() > old_score:
-        booster.save_model(model_file)
-        print("Сохранение модели")
-        print()
-        old_score = f1_score.mean()
-        with open('all_model_info.txt', 'a', encoding='utf-8') as f:
-            f.writelines("Новый F1 показатель = " + f"{f1_score:.10f}" + '\n')
+print('F1 score:', f1_score.mean())
+if f1_score.mean() > old_score:
+    booster.save_model(model_file)
+    print("Сохранение модели")
+    print()
+    old_score = f1_score.mean()
+    with open('all_model_info.txt', 'a', encoding='utf-8') as f:
+        f.writelines("Новый F1 показатель = " + f"{f1_score:.10f}" + '\n')
 
 
 
